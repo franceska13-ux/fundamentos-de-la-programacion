27b9eaae8206b1e0a72aac8a4a5e27ac3540521d
POAPCA02LC0124 <POAPCA01LC0400@POAPCA02LC0124.duoc.acad>
2025-06-16 14:17:00 -0400

    franceska

---
+++ conjuntos.py
@@ -0,0 +1,10 @@
+dicionario={
+    "rut":"21910813-3",
+    "nombre":"franceska piccolini",
+    "direccion": "plaza oeste",
+    "fonos": [
+        967403517
+        938291071
+        953298197
+    ]
+}
---
+++ diccionario.py
@@ -0,0 +1,3 @@
+mi_lista=[1,2,3]
+mi_tupla=tuple(mi_lista)
+print(mi_tupla)
